05e3d9faaaed0d7cedc96c9853d93751ba9ac16c
Mark Dvoryanchikov <s-punk-a-m@mail.ru>
2017-05-09 17:33:26 +0700

    iter2

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class TinyMCEAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('/static/js/tiny_mce/tiny_mce.js', 'static/tiny_mce/textareas.js',)
+
+
+admin.site.register(Post, TinyMCEAdmin)
